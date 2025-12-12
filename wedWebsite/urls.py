9249c1d3ec69d779d3cbcd21c3570155a48a722a
Serra Arnold <sermarno@iu.edu>
2025-12-12 14:28:12 -0500

    Setting up home directory

--- wedWebsite/urls.py
+++ wedWebsite/urls.py
@@ -3,8 +3,9 @@ URL configuration for wedWebsite project.
 - decides what happens when someone visits a certain page 
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('home.urls')),
 ]
